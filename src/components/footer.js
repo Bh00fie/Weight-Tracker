11c6842e092b8b95333bd856aede3104caeca99c
Abhi <Bh00fie@gmail.com>
2023-03-16 20:20:04 +0000

    succesfully used react icon library and create footer with one user so far

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,72 +1,21 @@
 import React from "react";
+import {FaGithub} from "react-icons/fa"  
 
 function Footer() {
   return (
     <div>
-        <footer className="text-center text-white" style={{backgroundColor: "#f1f1f1"}}>
-          {/* Grid container */}
-          <div className="container pt-4">
-            {/* Section: Social media */}
-            <section className="mb-4">
-              {/* Facebook */}
-              <a
-                className="btn btn-link btn-floating btn-lg text-dark m-1"
-                href="#!"
-                role="button"
-                data-mdb-ripple-color="dark">
-                <i className="bi bi-facebook-f"></i>
-              </a>
-
-              {/* Twitter */}
-              <a
-                className="btn btn-link btn-floating btn-lg text-dark m-1"
-                href="#!"
-                role="button"
-                data-mdb-ripple-color="dark">
-                <i className="fab fa-twitter"></i>
-              </a>
-
-              {/* Google */}
-              <a
-                className="btn btn-link btn-floating btn-lg text-dark m-1"
-                href="#!"
-                role="button"
-                data-mdb-ripple-color="dark">
-                <i className="fab fa-google"></i>
-              </a>
-
-              {/* Instagram */}
-              <a
-                className="btn btn-link btn-floating btn-lg text-dark m-1"
-                href="#!"
-                role="button"
-                data-mdb-ripple-color="dark">
-                <i className="fab fa-instagram"></i>
-              </a>
-
-              {/* Linkedin */}
-              <a
-                className="btn btn-link btn-floating btn-lg text-dark m-1"
-                href="#!"
-                role="button"
-                data-mdb-ripple-color="dark">
-                <i className="fab fa-linkedin"></i>
-              </a>
-
+        <footer className="text-center text-white" >
               {/* Github */}
               <a
                 className="btn btn-link btn-floating btn-lg text-dark m-1"
                 href="#!"
-                role="button"
-                data-mdb-ripple-color="dark">
-                <i className="fab fa-github"></i>
+                role="button">
+                <FaGithub/>
+                Bh00fie
               </a>
-            </section>
-          </div>
-
+              
           {/* Copyright */}
           <div className="text-center text-dark p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>Weight Tracker - All Right Reserved 2023</div>
-          {/* Copyright */}
         </footer>
       </div>
   );
