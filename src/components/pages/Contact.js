419b608e3ecfd26f26b28b6ce2f9329e7fa8be33
Abhi <Bh00fie@gmail.com>
2023-03-17 18:43:55 +0000

    linked emailJS form to email account so I can receive emails

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import Learn from './Learn';
 import emailjs from '@emailjs/browser';
@@ -9,14 +9,14 @@ function Contact(props) {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm('service_wo1kije', 'template_19vdx6m', form.current, 'iBYhqT4UJAXbXb877')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
   };
-  
+
   return (
     <div>
       <h1>Get in touch with us:</h1>
