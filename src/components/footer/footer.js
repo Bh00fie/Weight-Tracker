722e6d635bac3ee9693e07dee08586dd287cb34d
Abhi <Bh00fie@gmail.com>
2023-03-19 00:40:44 +0000

    added styling for footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,13 +1,15 @@
 import React from "react";
 import {FaGithub} from "react-icons/fa"  
+import './footer.css'
 
 function Footer() {
   return (
     <div>
-        <footer className="text-center text-white" >
-              {/* Github */}
+        <footer>
+          {/* GitHub */}
+            <div className="gitHubLinks">
               <a
-                className="btn btn-link btn-floating btn-lg text-dark m-1"
+                className="btn btn-floating btn-lg text-light m-1"
                 href="https://github.com/Bh00fie"
                 role="button">
                 <FaGithub/>
@@ -15,7 +17,7 @@ function Footer() {
               </a>
 
               <a
-                className="btn btn-link btn-floating btn-lg text-dark m-1"
+                className="btn btn-floating btn-lg text-light m-1"
                 href="https://github.com/DrGoshD"
                 role="button">
                 <FaGithub/>
@@ -23,7 +25,7 @@ function Footer() {
               </a>
 
               <a
-                className="btn btn-link btn-floating btn-lg text-dark m-1"
+                className="btn btn-floating btn-lg text-light m-1"
                 href="https://github.com/rzepa000"
                 role="button">
                 <FaGithub/>
@@ -31,15 +33,16 @@ function Footer() {
               </a>
 
               <a
-                className="btn btn-link btn-floating btn-lg text-dark m-1"
+                className="btn btn-floating btn-lg text-light m-1"
                 href="https://github.com/Allen-EC"
                 role="button">
                 <FaGithub/>
                 Allen Cummins
               </a>
+            </div>
               
           {/* Copyright */}
-          <div className="text-center text-dark p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>Weight Tracker - All Right Reserved 2023</div>
+          <div className="copyright text-light" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>Weight Tracker - All Right Reserved 2023</div>
         </footer>
       </div>
   );
