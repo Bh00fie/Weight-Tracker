805361011b7d52ccfb1f7108cc0dd4af0472a354
Abhi <Bh00fie@gmail.com>
2023-03-20 21:25:47 +0000

    added function that uses dummy user data to generate graph

--- src/components/pages/Tracker.js
+++ src/components/pages/Tracker.js
@@ -4,6 +4,26 @@ import LineChart from "..section/linechart/LineChart.js";
 import { useState } from "react";
 
 function Tracker() {
+
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+  
   return (
     <div>
       <h1>Weight Tracker</h1>
