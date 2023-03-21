a533938374daea9c6009516603313a79f9a48844
Allen-EC <117396091+Allen-EC@users.noreply.github.com>
2023-03-21 20:47:27 +0000

    now storing latest Weight and Date in local storage

--- src/components/pages/Tracker.js
+++ src/components/pages/Tracker.js
@@ -1,28 +1,25 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { UserData } from "../sections/linechart/Data";
 import LineChart from "../sections/linechart/LineChart";
 import "./tracker.css"
 
-const Userweight = () => {
-  return (
-    <form>
-      <div>
-        <label htmlFor="NewWeight">Weight:</label>
-        <input id="NewWeight" type="number" />
-      </div>
-      <div>
-        <label htmlFor="NewDate">Date</label>
-        <input id="NewDate" type="date" />
-      </div>
-      <button className="submitButton" >Submit</button>
-    </form>
-  )
-};
+    
+      
 
-function Tracker() {
+const Tracker = () => {
 
-  const [userData, setUserData] = useState({
+  const [weight, setWeight] = useState(() => {
+    const savedWeight = localStorage.getItem("weight");
+    const initialValue = JSON.parse(savedWeight);
+    return initialValue || "";
+  });
+  const [date, setDate] = useState(() => {
+    const savedDate = localStorage.getItem("date");
+    const initialValue = JSON.parse(savedDate);
+    return initialValue || "";
+  });
+  const [userData, setUserData ] = useState({
     labels: UserData.map((data) => data.year),
     datasets: [
       {
@@ -40,7 +37,17 @@ function Tracker() {
       },
     ],
   });
+
+  useEffect(() => {
+    // storing input name
+    localStorage.setItem("weight", JSON.stringify(weight));
+  }, [weight]);
+  useEffect(() => {
+    // storing input name
+    localStorage.setItem("date", JSON.stringify(date));
+  }, [date]);
   
+
   return (
     <div className="App">
       <h1>Weight Tracker</h1>
@@ -48,11 +55,22 @@ function Tracker() {
       <form>
       <div className="weightInput">
         <label htmlFor="NewWeight">Weight:</label>
-        <input id="NewWeight" type="number" />
+        <input id="NewWeight"
+        type="number"
+        value={weight}
+        onChange={(e) => setWeight(e.target.value)}
+        placeholder=""
+        />
+        {/* <input type="submit" value="Submit"></input> */}
       </div>
       <div className="dateInput">
         <label htmlFor="NewDate">Date</label>
-        <input id="NewDate" type="date" />
+        <input id="NewDate"
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        placeholder=""
+        />
       </div>
       <br></br>
       <button className="submitButton">Submit</button>
@@ -64,12 +82,12 @@ function Tracker() {
     </div>
 
   );
-}
+};
 
-{/* <label htmlFor={'upload-button'}>
-    <div className={classes.chooseFile}>
-        <SomeIconElement style={{marginRight: 10}}/> Upload File
-    </div>
-</label> */}
+// {/* <label htmlFor={'upload-button'}>
+//     <div className={classes.chooseFile}>
+//         <SomeIconElement style={{marginRight: 10}}/> Upload File
+//     </div>
+// </label> */}
 
 export default Tracker;
