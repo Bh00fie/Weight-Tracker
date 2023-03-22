767350b6709ea392d357d0aff2e675fa14518496
Abhi <Bh00fie@gmail.com>
2023-03-22 19:18:55 +0000

    update Contact JS so when the form has been submitted it will show a confirmation of the email sent

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,37 +1,44 @@
-import React, {useRef} from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
-import "./contactUs.css"
+import './contactUs.css';
 
 function Contact(props) {
   const form = useRef();
+  const [isEmailSent, setIsEmailSent] = useState(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_wo1kije', 'template_19vdx6m', form.current, 'iBYhqT4UJAXbXb877')
+    emailjs
+      .sendForm('service_wo1kije', 'template_19vdx6m', form.current, 'iBYhqT4UJAXbXb877')
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
+        setIsEmailSent(true);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
   };
 
   return (
-    <div className='formBox'>
-      <h1>Get in touch with us:</h1>
-      
-      <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" className='sendButton'/> 
-    </form>
+    <div className='contactContainer'>
+      {isEmailSent ? (
+        <div className='confirmationMessage'>
+          <p>Email sent successfully!</p>
+        </div>
+      ) : (
+        <form ref={form} onSubmit={sendEmail}>
+          <h1>Get in touch with us:</h1>
+          <label>Name</label>
+          <input type='text' name='user_name' />
+          <label>Email</label>
+          <input type='email' name='user_email' />
+          <label>Message</label>
+          <textarea name='message' />
+          <input type='submit' value='Send' className='sendButton' />
+        </form>
+      )}
     </div>
   );
 }
 
 export default Contact;
-
